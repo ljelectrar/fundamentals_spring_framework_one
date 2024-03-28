32eca1af7c3afe78188770146ef3fdfdc1694c16
ljelectrar <ljelectrar@gmail.com>
2024-03-28 08:23:50 -0300

    Start

---
+++ src/main/java/com/ljelectrar/WorkHardWithSpringFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.ljelectrar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkHardWithSpringFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkHardWithSpringFundamentalsApplication.class, args);
+	}
+
+}
