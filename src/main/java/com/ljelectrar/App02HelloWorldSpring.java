6938cff3288c87ceb0aaee4d5dba033298eb65ac
ljelectrar <ljelectrar@gmail.com>
2024-03-29 10:53:26 -0300

    Mr bean with @Beans

--- src/main/java/com/ljelectrar/App02HelloWorldSpring.java
+++ src/main/java/com/ljelectrar/App02HelloWorldSpring.java
@@ -9,8 +9,9 @@ public class App02HelloWorldSpring {
 		var context = 
 			new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 		
+		System.out.println(context.getBean("name"));
+		System.out.println(context.getBean("age"));
 		System.out.println(context.getBean("mrBean"));
-		
 	}
 
 }
