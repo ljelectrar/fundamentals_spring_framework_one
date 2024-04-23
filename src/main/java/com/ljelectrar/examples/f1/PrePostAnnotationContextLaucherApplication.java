c70cead7484b94b2fe0eac464154c312bc93ac23
ljelectrar <ljelectrar@gmail.com>
2024-04-23 08:28:43 -0300

    PostConstruct and PreDestroy annotation

--- src/main/java/com/ljelectrar/examples/f1/PrePostAnnotationContextLaucherApplication.java
+++ src/main/java/com/ljelectrar/examples/f1/PrePostAnnotationContextLaucherApplication.java
@@ -1,9 +1,43 @@
 package com.ljelectrar.examples.f1;
 
+import java.util.Arrays;
+
 import org.springframework.beans.BeansException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+@Component
+class SomeClass {
+	private SomeDependency someDependency;
+
+	public SomeClass(SomeDependency someDependency) {
+		this.someDependency = someDependency;
+		System.out.println("All dependency are ready...");
+	}
+	
+	@PostConstruct
+	public void initialize() {
+		someDependency.getReady();
+	}
+	
+	@PreDestroy
+	public void cleanUp() {
+		System.out.println("Cleaning up...");
+	}
+}
+
+@Component
+class SomeDependency {
+
+	public void getReady() {
+		System.out.println("Some logic here using SomeDependency...");	
+	}
+}
 
 @Configuration
 @ComponentScan
@@ -12,8 +46,8 @@ public class PrePostAnnotationContextLaucherApplication {
 	public static void main(String[] args) {
 
 		try (var context = new AnnotationConfigApplicationContext(PrePostAnnotationContextLaucherApplication.class)) {
-
 			
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 
 		} catch (BeansException e) {
 			e.printStackTrace();
